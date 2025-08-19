c86b27b3700d0a6278dead1c0c0d440a7813b23c
reinvvay <reinvvay@gmail.com>
2025-08-19 22:01:01 +0300

    Minor model tweaks and admin customization

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,75 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers",
+        null=True
+    )
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "Worker"
+        verbose_name_plural = "Workers"
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ("Urgent", "Urgent"),
+        ("High Priority", "High Priority"),
+        ("Medium Priority", "Medium Priority"),
+        ("Low Priority", "Low Priority"),
+        ("Optional", "Optional"),
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=50,
+        choices=PRIORITY_CHOICES,
+        default="Medium Priority"
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(
+        Worker,
+        related_name="tasks",
+        blank=True
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
